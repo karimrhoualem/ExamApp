f1e6b2b794ccfa558f8c34ef4ef0e02fd2ae6092
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-03-16 12:49:55 -0400

    Added textview and webview client

--- AndroidAPP/app/src/main/java/edu/coen390/androidapp/FaceRecognition.java
+++ AndroidAPP/app/src/main/java/edu/coen390/androidapp/FaceRecognition.java
@@ -3,15 +3,28 @@ package edu.coen390.androidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.example.examapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
 
 public class FaceRecognition extends AppCompatActivity {
 
     WebView myWebView;
+    TextView personTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +37,42 @@ public class FaceRecognition extends AppCompatActivity {
     private void webView(){
         myWebView = (WebView) findViewById(R.id.webView);
         myWebView = new WebView(FaceRecognition.this);
+
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest webResourceRequest) {
+                view.loadUrl(webResourceRequest.getUrl().toString());
+                return true;
+            }
+        });
+
         setContentView(myWebView);
+
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setUseWideViewPort(true);
         myWebView.loadUrl("http://192.168.2.135:5000/");
 
+        try {
+            setPersonTextView();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private void setPersonTextView() throws JSONException {
+        personTextView =(TextView) findViewById(R.id.personTextView);
+        String jsonData = "http://192.168.2.135:5000/person_info";
+        JSONObject reader = new JSONObject(jsonData);
+        JSONObject person = reader.getJSONObject("person");
+
+        String info = "Student Name : " + person.getString("name")
+                      + " Student ID :" + person.getString("ID");
+        personTextView.setText(info);
+
+    }
+
+
 }
