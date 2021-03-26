05326b84f8f62d94edc083ef407de2e0e65ed0e7
SuperThunder <alexander.wolfe.10@googlemail.com>
2021-03-26 00:45:39 -0400

    include 100 faces from LFW dataset

--- server-app/generate_encodings.py
+++ server-app/generate_encodings.py
@@ -11,6 +11,8 @@ import pickle
 FACE_INFO_FOLDER = "faces"  # relative to face_rec.py
 FACE_INFO_CONFIG = "face_info.json"
 
+LABELLED_FACES_FOLDER ="faces/big-faces-dataset"
+
 PICKLE_OUTPUT_FILE = "encodings.dat"
 
 #known_face_encodings = []
@@ -23,14 +25,13 @@ face_info['names'] = []
 face_info['ids'] = []
 face_info['encodings'] = []
 
-
+start_time = timeit.default_timer()
+print("Creating face encodings")
 
 # load the face info
 def load_face_info():
     # face_info = []
     # get the relations between image file and people
-    start_time = timeit.default_timer()
-    print("Creating face encodings")
 
     dirname = os.path.dirname(__file__)
     index_file_path = os.path.join(dirname, FACE_INFO_FOLDER, FACE_INFO_CONFIG)
@@ -51,11 +52,42 @@ def load_face_info():
             face_info['ids'].append(person['ID'])
 
 
-        stop_time = timeit.default_timer()
-        print("Time to load faces: {time}\n".format(time=(stop_time - start_time)))
+# load some faces from the labelled faces in the wild dataset
+def load_face_extra_dataset():
+    dirname = os.getcwd()
 
+    dataset_folder_path = os.path.join(dirname, LABELLED_FACES_FOLDER, "")
 
+    dataset_folders = os.listdir(dataset_folder_path)
+
+    # Will use first 100 people
+    # TODO: Choose randomly instead?
+    print("First 100 people:")
+    print(dataset_folders[0:100])
+
+    for person in dataset_folders[0:100]:
+        print("Load face info for {name}".format(name=person))
+        face_file_folder = os.path.join(dataset_folder_path, person)
+
+        # some of the LFW folders have multiple faces, choose the first
+        face_file_path = os.path.join(face_file_folder, os.listdir(face_file_folder)[0])
+        #print(face_file_path)
+
+        person_image = face_recognition.load_image_file(face_file_path)
+        person_face_encoding = face_recognition.face_encodings(person_image)[0]
+
+        face_info['encodings'].append(person_face_encoding)
+        face_info['names'].append(person)
+        face_info['ids'].append("0000")
+
+# face info for our images
 load_face_info()
+
+# faces from the labelled faces in the wild dataset, to make the dataset larger
+load_face_extra_dataset()
+
+stop_time = timeit.default_timer()
+print("Time to load faces: {time}\n".format(time=(stop_time - start_time)))
 
 pickle_file_path = os.path.join(FACE_INFO_FOLDER, PICKLE_OUTPUT_FILE)
 with open(pickle_file_path, 'wb') as picklefile:
