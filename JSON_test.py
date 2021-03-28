2e631e40e47b0340b4c4b067d0af4787a96466a9
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-03-28 16:47:07 -0400

    misc files and gradle

--- JSON_test.py
+++ JSON_test.py
@@ -8,4 +8,4 @@ def send():
 
 # Run from command line using flask run --host="<IP Address>" --port=<portNumber>
 if __name__ == "__main__":
-    app.run()
+    app.run(host="192.168.2.135", port= "5000")
