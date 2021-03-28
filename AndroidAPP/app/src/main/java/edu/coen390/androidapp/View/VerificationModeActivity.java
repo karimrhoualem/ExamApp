b37dfc4b63f695dd1a7d1bb9e9ee62dc557c591a
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-03-28 16:27:51 -0400

    create exam table per course object

--- AndroidAPP/app/src/main/java/edu/coen390/androidapp/View/VerificationModeActivity.java
+++ AndroidAPP/app/src/main/java/edu/coen390/androidapp/View/VerificationModeActivity.java
@@ -12,6 +12,7 @@ import com.example.examapp.R;
 
 import java.io.Serializable;
 
+import edu.coen390.androidapp.Controller.Config;
 import edu.coen390.androidapp.Controller.DatabaseHelper;
 import edu.coen390.androidapp.Model.Course;
 
@@ -37,26 +38,30 @@ public class VerificationModeActivity extends AppCompatActivity implements Seria
         Intent intent = getIntent();
         course = (Course)intent.getSerializableExtra(COURSE_INTENT);
         Log.d(TAG,"After getIntent "+ course.toString());
-
-        dbHelper = new DatabaseHelper(this);
-        dbHelper.createExamTable(course);
+        createExamTable();
 
         setupUI();
     }
 
     private void setupUI() {
-        facialRecognitionButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(VerificationModeActivity.this, LiveFeedActivity.class);
-                intent.putExtra(COURSE_INTENT, course);
-                startActivity(intent);
-            }
+        facialRecognitionButton.setOnClickListener(v -> {
+            Intent intent = new Intent(VerificationModeActivity.this, LiveFeedActivity.class);
+            intent.putExtra(COURSE_INTENT, course);
+            startActivity(intent);
         });
 
-        cardScannerButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // TODO: empty for now. To be completed in sprint 3 when we receive the card reader.
-            }
+        cardScannerButton.setOnClickListener(v -> {
+            // TODO: empty for now. To be completed in sprint 3 when we receive the card reader.
         });
+    }
+
+    private void createExamTable(){
+
+        String courseExamTableName = course.getCode().
+                replaceAll("\\s+", "") + "Exam";
+        Config.EXAM_TABLE_NAME.put(course.getCode(), courseExamTableName);
+        dbHelper = new DatabaseHelper(this);
+        dbHelper.createExamTable(course);
+
     }
 }
