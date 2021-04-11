069e1d4b7b0905f42bca0fccdb0904c773e6a065
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-04-11 16:39:14 -0400

    added Comments to seperate tables

--- AndroidAPP/app/src/main/java/edu/coen390/androidapp/Controller/Config.java
+++ AndroidAPP/app/src/main/java/edu/coen390/androidapp/Controller/Config.java
@@ -6,8 +6,11 @@ import java.util.Hashtable;
  * Configuration class that holds the db table and column names.
  */
 public class Config {
+    /* =================== Database Name =================== */
     public static final String DATABASE_NAME = "ExamApp-DB";
 
+
+    /* =================== Invigilator Table =================== */
     public static final String INVIGILATOR_TABLE_NAME = "Invigilators";
     public static final String INVIGILATOR_ID = "InvigilatorID";
     public static final String INVIGILATOR_COURSES = "InvigilatorCourses";
@@ -16,6 +19,8 @@ public class Config {
     public static final String INVIGILATOR_FIRST_NAME = "InvigilatorFirstName";
     public static final String INVIGILATOR_LAST_NAME = "InvigilatorLastName";
 
+
+    /* =================== Professors Table=================== */
     public static final String PROFESSOR_TABLE_NAME = "Professors";
     public static final String PROFESSOR_ID = "ProfessorID";
     public static final String PROFESSOR_COURSES = "ProfessorCourses";
@@ -24,6 +29,8 @@ public class Config {
     public static final String PROFESSOR_FIRST_NAME = "ProfessorFirstName";
     public static final String PROFESSOR_LAST_NAME = "ProfessorLastName";
 
+
+    /* =================== Course Table =================== */
     public static final String COURSE_TABLE_NAME = "Courses";
     public static final String COURSE_ID = "CourseID";
     public static final String COURSE_TITLE = "CourseTitle";
@@ -31,6 +38,8 @@ public class Config {
     public static final String COURSE_DESCRIPTION = "CourseDescription";
     public static final String COURSE_TOTAL_ENROLLED = "CourseTotalEnrolled";
 
+
+    /* =================== Students Table =================== */
     public static final String STUDENTS_TABLE_NAME = "Students";
     public static final String STUDENT_ID = "StudentID";
     public static final String STUDENT_COURSES = "StudentCourses";
@@ -39,6 +48,8 @@ public class Config {
     public static final String STUDENT_USERNAME = "StudentUsername";
     public static final String STUDENT_PASSWORD = "StudentPassword";
 
+
+    /* =================== Exam Table =================== */
     public static final Hashtable<String, String> EXAM_TABLE_NAME = new Hashtable<>();
     public static final String EXAM_ID = "ExamID";
     public static final String EXAM_COURSE_ID = "ExamCourseID";
