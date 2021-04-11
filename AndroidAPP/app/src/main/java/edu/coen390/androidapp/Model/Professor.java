0b70e2426c2a1ace2d06c656dfdb870239ebe8f6
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-04-11 17:15:57 -0400

    code cleanup

--- AndroidAPP/app/src/main/java/edu/coen390/androidapp/Model/Professor.java
+++ AndroidAPP/app/src/main/java/edu/coen390/androidapp/Model/Professor.java
@@ -3,7 +3,9 @@ package edu.coen390.androidapp.Model;
 import java.io.Serializable;
 
 public class Professor extends User implements Serializable {
-    public Professor() {}
+    public Professor() {
+    }
+
     public Professor(long id, String[] courses, String firstName, String lastName, String userName, String password) {
         super(id, courses, firstName, lastName, userName, password);
     }
