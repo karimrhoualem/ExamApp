53887579cf5e46803977fefcc6d3bf411556e738
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-04-11 17:00:46 -0400

    1 - fixed imports, 2 - code cleanup

--- AndroidAPP/app/src/main/java/edu/coen390/androidapp/Model/Invigilator.java
+++ AndroidAPP/app/src/main/java/edu/coen390/androidapp/Model/Invigilator.java
@@ -3,7 +3,10 @@ package edu.coen390.androidapp.Model;
 import java.io.Serializable;
 
 public class Invigilator extends User implements Serializable {
-    public Invigilator() { super(); }
+    public Invigilator() {
+        super();
+    }
+
     public Invigilator(long id, String[] courses, String firstName, String lastName, String userName, String password) {
         super(id, courses, firstName, lastName, userName, password);
     }
