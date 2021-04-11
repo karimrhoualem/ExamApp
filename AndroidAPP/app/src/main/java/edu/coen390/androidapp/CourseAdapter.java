e8d3cbe05f64a43034d4ac9ef5cf694cdfbb3a88
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-04-11 17:00:32 -0400

    1 - fixed imports, 2 - code cleanup

--- AndroidAPP/app/src/main/java/edu/coen390/androidapp/CourseAdapter.java
+++ AndroidAPP/app/src/main/java/edu/coen390/androidapp/CourseAdapter.java
@@ -7,11 +7,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.examapp.R;
-
 import java.util.List;
 
 import edu.coen390.androidapp.Model.Course;
