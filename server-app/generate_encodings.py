aab9c13459120d45a5783659f70f9686fd069be9
Ahmed Ali <ahmed.ali@mail.concordia.ca>
2021-04-12 04:19:53 -0400

    added rotate_images method and generated new encodings

--- server-app/generate_encodings.py
+++ server-app/generate_encodings.py
@@ -5,6 +5,14 @@ import random
 import timeit
 
 import face_recognition
+import image_to_numpy
+
+import PIL.Image
+import PIL.ImageOps
+import numpy as np
+from PIL import Image, ExifTags
+
+# {"name": "Paulina Aviles",  "ID": "40109825", "folder": "paulina"},
 
 FACE_INFO_FOLDER = "faces"  # relative to face_rec.py
 FACE_INFO_CONFIG = "face_info.json"
@@ -34,6 +42,8 @@ def load_face_info():
             print("Load face info for {name}".format(name=person['name']))
             # assume images for now to be in, eg, faces/obama/obama.jpg
             face_file_path = os.path.join(dirname, FACE_INFO_FOLDER, person['folder'], person['folder'] + '.jpg')
+            #img = image_to_numpy.load_image_file(face_file_path)
+            rotate_images(face_file_path)
             person_image = face_recognition.load_image_file(face_file_path)
             person_face_encoding = face_recognition.face_encodings(person_image, num_jitters=100)[0]
 
@@ -65,6 +75,8 @@ def load_face_extra_dataset():
         # print(face_file_path)
 
         try:
+            #img = image_to_numpy.load_image_file(face_file_path)
+            rotate_images(face_file_path)
             person_image = face_recognition.load_image_file(face_file_path)
             person_face_encoding = face_recognition.face_encodings(person_image)[0]
 
@@ -75,6 +87,61 @@ def load_face_extra_dataset():
         except Exception as e:
             print("ERROR: Could not encode face for {name}".format(name=person))
             print(e)
+
+#Rotate images to correct orientation
+#References :
+# 1 - https://stackoverflow.com/questions/13872331/rotating-an-image-with-orientation-specified-in-exif-using-python-without-pil-in
+# 2 - https://medium.com/@ageitgey/the-dumb-reason-your-fancy-computer-vision-app-isnt-working-exif-orientation-73166c7d39da
+def exif_transpose(img):
+    if not img:
+        return img
+
+    exif_orientation_tag = 274
+
+    # Check for EXIF data (only present on some files)
+    if hasattr(img, "_getexif") and isinstance(img._getexif(), dict) and exif_orientation_tag in img._getexif():
+        exif_data = img._getexif()
+        orientation = exif_data[exif_orientation_tag]
+
+        # Handle EXIF Orientation
+        if orientation == 1:
+            # Normal image - nothing to do!
+            pass
+        elif orientation == 2:
+            # Mirrored left to right
+            img = img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 3:
+            # Rotated 180 degrees
+            img = img.rotate(180)
+        elif orientation == 4:
+            # Mirrored top to bottom
+            img = img.rotate(180).transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 5:
+            # Mirrored along top-left diagonal
+            img = img.rotate(-90, expand=True).transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 6:
+            # Rotated 90 degrees
+            img = img.rotate(-90, expand=True)
+        elif orientation == 7:
+            # Mirrored along top-right diagonal
+            img = img.rotate(90, expand=True).transpose(PIL.Image.FLIP_LEFT_RIGHT)
+        elif orientation == 8:
+            # Rotated 270 degrees
+            img = img.rotate(90, expand=True)
+
+    return img
+
+
+def rotate_images(filepath):
+    try:
+        image = Image.open(filepath)
+        image = exif_transpose(image)
+
+        image.save(filepath)
+        image.close()
+    except (AttributeError, KeyError, IndexError):
+        # cases: image don't have getexif
+        pass
 
 
 # face info for our images
